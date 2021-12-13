8f5917247760144824982ab8f941d366f57f5d8a
strogera <26325577+strogera@users.noreply.github.com>
2021-12-13 13:26:51 +0200

    Day 13 improved, no grid

--- Day 13/d13.py
+++ Day 13/d13.py
@@ -1,90 +1,70 @@
 def partOne():
     with open("input.txt", "r") as inputFile:
         lines = inputFile.read()
-        elems,  folds = lines.split('\n\n')
-        elems = [ tuple(map(int, line.split(','))) for line in elems.split('\n')]
-        grid = []
-        for x in range(max(elems, key = lambda k: k[0])[0] + 1):
-            grid.append([' ']*(max(elems, key = lambda k: k[1])[1] + 1))
-
-        for marked in elems:
-            grid[marked[0]][marked[1]] = '#'
-
-        gridXLength = len(grid)
-        gridYLength = len(grid[0])
+        points,  folds = lines.split('\n\n')
+        points = set(tuple(map(int, line.split(','))) for line in points.split('\n'))
         for fold in folds.split('\n'):
             if '=' in fold:
                 index = int(fold.split('=')[-1])
             else:
                 continue
             if 'y' in fold:
-                for xx in range(gridXLength):
-                    for yy in range(gridYLength):
-                        if yy > index:
-                            if grid[xx][yy] == '#':
-                                grid[xx][index - (yy - index)]  = '#'
-                                grid[xx][yy] = ' '
-                gridYLength = index
+                newPoints = set()
+                while points:
+                    point = points.pop()
+                    if point[1] > index:
+                        point = (point[0], index - (point[1] - index))
+                    newPoints.add(point)
+                points = newPoints
 
             elif 'x' in fold:
-                for xx in range(gridXLength):
-                    for yy in range(gridYLength):
-                        if xx > index:
-                            if grid[xx][yy] == '#':
-                                grid[index - (xx - index)][yy] = '#'
-                                grid[xx][yy] = ' '
-                gridXLength = index
+                newPoints = set()
+                while points:
+                    point = points.pop()
+                    if point[0] > index:
+                        point = (index - (point[0] - index), point[1])
+                    newPoints.add(point)
+                points = newPoints
             break
-        count = 0
-        for x in range(gridXLength):
-            for y in range(gridYLength):
-                if grid[x][y] == '#':
-                    count += 1
-        return count
+        return len(points)
 
 
 def partTwo():
     with open("input.txt", "r") as inputFile:
         lines = inputFile.read()
-        elems,  folds = lines.split('\n\n')
-        elems = [ tuple(map(int, line.split(','))) for line in elems.split('\n')]
-        grid = []
-        for x in range(max(elems, key = lambda k: k[0])[0] + 1):
-            grid.append([' ']*(max(elems, key = lambda k: k[1])[1] + 1))
-
-        for marked in elems:
-            grid[marked[0]][marked[1]] = '#'
-
-        gridXLength = len(grid)
-        gridYLength = len(grid[0])
+        points,  folds = lines.split('\n\n')
+        points = set(tuple(map(int, line.split(','))) for line in points.split('\n'))
         for fold in folds.split('\n'):
             if '=' in fold:
                 index = int(fold.split('=')[-1])
             else:
                 continue
             if 'y' in fold:
-                for xx in range(gridXLength):
-                    for yy in range(gridYLength):
-                        if yy > index:
-                            if grid[xx][yy] == '#':
-                                grid[xx][index - (yy - index)]  = '#'
-                                grid[xx][yy] = ' '
-                gridYLength = index
+                newPoints = set()
+                while points:
+                    point = points.pop()
+                    if point[1] > index:
+                        point = (point[0], index - (point[1] - index))
+                    newPoints.add(point)
+                points = newPoints
 
             elif 'x' in fold:
-                for xx in range(gridXLength):
-                    for yy in range(gridYLength):
-                        if xx > index:
-                            if grid[xx][yy] == '#':
-                                grid[index - (xx - index)][yy] = '#'
-                                grid[xx][yy] = ' '
-                gridXLength = index
-        for y in range((gridYLength)):
+                newPoints = set()
+                while points:
+                    point = points.pop()
+                    if point[0] > index:
+                        point = (index - (point[0] - index), point[1])
+                    newPoints.add(point)
+                points = newPoints
+
+        for y in range((max(points, key = lambda k: k[1])[1] + 1)):
             l = ''
-            for x in range((gridXLength)):
-                l += grid[x][y]
+            for x in range(max(points, key = lambda k: k[0])[0] + 1):
+                if (x, y) in points:
+                    l += '#'
+                else:
+                    l += ' '
             print(l)
-        print()
 
 print("Answer for part 1: ")
 print(partOne())
