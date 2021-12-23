b5d83f41d2475ae09bf5927d47e18fc37e12aeb4
strogera <26325577+strogera@users.noreply.github.com>
2021-12-23 14:08:20 +0200

    Day 23

---
+++ Day 23/d23.py
@@ -0,0 +1,11 @@
+def partOne():
+    return 15338
+
+def partTwo():
+    return 47064
+
+print("Solved by hand, answers for my input:")
+print("Answer for part 1: ")
+print(partOne())
+print("Answer for part 2: ")
+print(partTwo())
